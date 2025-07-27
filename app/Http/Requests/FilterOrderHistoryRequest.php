b3a211c4d81d7a447d39d50a16ff5dccb1962322
William Valerie <williamvalerie2@gmail.com>
2025-07-27 16:37:24 +0700

    Refactor OrderController@index to use FilterOrderHistoryRequest and sorted the order shown by created_at

---
+++ app/Http/Requests/FilterOrderHistoryRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\UserRole;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class FilterOrderHistoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check() && Auth::user()->role === UserRole::Customer;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => [
+                'nullable',
+                'string',
+                Rule::in(['active', 'upcoming', 'cancelled', 'finished', 'all']),
+            ],
+            'query' => [
+                'nullable',
+                'string',
+                'max:255',
+            ]
+        ];
+    }
+}
