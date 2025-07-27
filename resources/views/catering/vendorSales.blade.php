262016fe2f74c1e8cb5a407158802d20ae47099b
William Valerie <williamvalerie2@gmail.com>
2025-07-27 12:27:35 +0700

    Add en, id lang for salesTable and vendorSales view

--- resources/views/catering/vendorSales.blade.php
+++ resources/views/catering/vendorSales.blade.php
@@ -15,7 +15,7 @@
 @section('content')
     {{-- TITLE --}}
     <section class="container mt-3">
-        <h2 class="fw-bold text-center">{{ $vendor->name }}'s Sales</h2>
+        <h2 class="fw-bold text-center">{{__('catering/sales.sales_head', ['name' => $vendor->name])}}</h2>
     </section>
 
     {{-- FILTER CONTAINER --}}
@@ -23,12 +23,12 @@
         <form action="{{ route('sales.show') }}" method="GET" class="d-flex flex-row flex-wrap gap-2 align-items-end">
             @csrf
             <div>
-                <label for="startDate" class="form-label mb-0">Start Date</label>
+                <label for="startDate" class="form-label mb-0">{{__('catering/sales.start_date')}}</label>
                 <input type="date" name="startDate" id="startDate" class="form-control"
                     value="{{ request()->query('startDate') }}" max="{{ now()->format('Y-m-d') }}">
             </div>
             <div>
-                <label for="endDate" class="form-label mb-0">End Date</label>
+                <label for="endDate" class="form-label mb-0">{{__('catering/sales.end_date')}}</label>
                 <input type="date" name="endDate" id="endDate" class="form-control"
                     value="{{ request()->query('endDate') }}" max="{{ now()->format('Y-m-d') }}">
             </div>
@@ -39,12 +39,12 @@
         <div>
             @if($orders->isEmpty())
                 <button class="btn btn-green" disabled>
-                    Export
+                    {{__('catering/sales.export')}}
                 </button>
             @else
                 <a href="{{ route('sales.export', ['startDate' => request()->query('startDate'), 'endDate' => request()->query('endDate')]) }}"
                     class="btn btn-green">
-                    Export
+                    {{__('catering/sales.export')}}
                 </a>
             @endempty
         </div>
@@ -58,7 +58,7 @@
         @if (!$orders->isEmpty())
             @include('catering.salesTable', [$orders, $totalSales, $startDate, $endDate])
         @else
-            <h4>No sales yet.</h4>
+            <h4>{{__('catering/sales.no_sales')}}</h4>
         @endif
     </div>
 @endsection
