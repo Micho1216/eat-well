912505d1ad7b0e7941d03551fb6288fa62905030
cynthiantoni <cynthia02antoni@gmail.com>
2025-08-01 07:30:32 +0700

    rename customer address test

--- tests/Feature/customerAddressTest.php
+++ tests/Feature/customerAddressTest.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class AddressManagementTest extends TestCase
+class CustomerAddressTest extends TestCase
 {
     use RefreshDatabase;
 
