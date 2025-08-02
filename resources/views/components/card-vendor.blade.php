5967ea94dc0a073b2fd5650566f54809b642e39b
William Valerie <williamvalerie2@gmail.com>
2025-08-02 13:25:57 +0700

    remove rating in card vendor if avg rating 0

--- resources/views/components/card-vendor.blade.php
+++ resources/views/components/card-vendor.blade.php
@@ -1,6 +1,6 @@
-<a href="{{ route('catering-detail', $vendor->vendorId) }}" 
-   class="catering-card-link position-relative {{ $tooFar ? 'too-far' : '' }}">
-    
+<a href="{{ route('catering-detail', $vendor->vendorId) }}"
+    class="catering-card-link position-relative {{ $tooFar ? 'too-far' : '' }}">
+
     @if ($tooFar)
         <div class="too-far-label">
             {{ __('customer/card-vendor.too_far') ?? 'Too Far' }}
@@ -9,9 +9,7 @@
 
     <div class="catering-card">
         <div class="catering-card-img-wrapper">
-            <img src="{{ asset('asset/vendorLogo/' . $vendor->logo) }}" 
-                 alt="Catering Picture" 
-                 class="catering-card-img">
+            <img src="{{ asset('asset/vendorLogo/' . $vendor->logo) }}" alt="Catering Picture" class="catering-card-img">
         </div>
         <div class="catering-card-body d-flex flex-column flex-grow-1">
             <div class="d-flex justify-content-between align-items-center">
@@ -30,10 +28,12 @@
                     <span class="badge badge-{{ $slot }}">{{ __('slot.' . $slot) }}</span>
                 @endforeach
             </div>
-            <div class="catering-rating d-flex align-items-center">
-                <span class="material-symbols-outlined star-icon me-1">star</span>
-                <span class="fw-semibold">{{ $vendor->rating ?? '-' }}</span>
-            </div>
+            @if ($vendor->rating > 0)
+                <div class="catering-rating d-flex align-items-center">
+                    <span class="material-symbols-outlined star-icon me-1">star</span>
+                    <span class="fw-semibold">{{ $vendor->rating }}</span>
+                </div>
+            @endif
         </div>
     </div>
 </a>
