94064ed3784a56acc0e23481c4d1f288afba78a6
DvNAlvN <davialviano79@gmail.com>
2025-08-03 02:13:39 +0700

    qa

---
+++ tests/Feature/CancelOrderCustomerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Address;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Payment;
+use App\Models\PaymentMethod;
+use App\Models\Vendor;
+
+class CancelOrderCustomerTest extends TestCase
+{
+    // use RefreshDatabase;
+
+    /** @test */
+    /** @test */
+    public function user_can_cancel_order_and_get_wellpay_refunded()
+    {
+        
+    }
+}
